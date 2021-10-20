540660a4d3075cd7a704f53db818e7d58884d960
Taylor Laing <taylor.laing@maloufcompanies.com>
2021-10-20 14:12:41 -0600

    Add yotpo file structure

---
+++ marketing/yotpo/index.js
@@ -0,0 +1,12 @@
+import Api from './api'
+import Repository from './repository'
+import Service from './service'
+
+export default function(context){
+    
+    const api = new Api(context.api)
+    const repository = new Repository(context.store, api)
+    const service = new Service(api)
+
+    return service
+}
