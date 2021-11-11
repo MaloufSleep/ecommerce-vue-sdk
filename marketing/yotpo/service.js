69049e0643a3ab7b9d62c065b50c2a749fbe0e23
Taylor Laing <taylor.laing@maloufcompanies.com>
2021-11-11 09:02:30 -0700

    Order Track Script

--- marketing/yotpo/service.js
+++ marketing/yotpo/service.js
@@ -30,11 +30,47 @@ export default class YotpoService {
             script.async = true
             script.defer = true
             script.src = src
+            script.id = 'yotpo'
             tag.parentNode.insertBefore(script, tag)
         })
     }
 
+    /**
+     * Loads the Yotpo order conversion tracking script on the confirmation page after order is complete
+     */
+    loadOrderScript(order) {
+        return new Promise((resolve, reject) => {
+            // Order
+            const script = document.createElement('script')
+            const tag = document.getElementsByTagName('script')[0]
+
+            document.body.appendChild(script)
+            script.onload = resolve
+            script.onerror = reject
+            // script.async = true
+            script.defer = true
+            script.innerHTML = `yotpoTrackConversionData = {orderId: '${order.order_id}',orderAmount: '${order.total}',orderCurrency: '${order.currency}'}`
+            tag.parentNode.insertBefore(script, tag)
+
+            // Img pixel
+            const noScript = document.createElement('noscript')
+            const tagNS = document.getElementsByTagName('noscript')[0]
+
+            document.body.appendChild(script)
+            noScript.onload = resolve
+            noScript.onerror = reject
+            noScript.innerHTML = `<img src='//api.yotpo.com/conversion_tracking.gif?app_key=${this.appKey}&order_id=${order.order_id}&order_amount=${order.total}&order_currency=${order.currency}' width='1' height='1'>`
+            tagNS.parentNode.insertBefore(noScript, tagNS)
+        })
+    }
+
     trackEvent(event, eventData){
-        return this.repository.trackEvent(event, eventData)
+        return this.repository.trackEvent(event, eventData).then(res => {
+            let order = this.$ecommerce.checkout.getOrder();
+
+            console.log('trackEvent Data', res.data);
+            console.log('order data', order);
+            this.loadOrderScript(order);
+        })
     }
 }
