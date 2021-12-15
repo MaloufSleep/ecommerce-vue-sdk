3ea3b7472d64975b8b7a8f135fe7a92c07295e10
Taylor Laing <taylor.laing@maloufcompanies.com>
2021-12-15 10:15:59 -0700

    add link to setpay

--- checkout/methods/financing/setpay/setpay.js
+++ checkout/methods/financing/setpay/setpay.js
@@ -26,7 +26,7 @@ export default class SetPay {
         console.log('load script', amount);
         if(!isClient()) return Promise.resolve(true)
         console.log('Is client load script')
-        const src = this.environment === 'production' ? '' : `https://bnpl.syf.com/widget/syf-widget-loader.js?partnerId=${this.partnerId}&purchaseAmount=${amount}`
+        const src = this.environment === 'production' ? `https://bnpl.syf.com/widget/syf-widget-loader.js?partnerId=${this.partnerId}&purchaseAmount=${amount}` : `https://qbnpl.syf.com/widget/syf-widget-loader.js?partnerId=${this.partnerId}&purchaseAmount=${amount}`
         console.log('source', src)
         return new Promise((resolve, reject) => {
             const script = document.createElement('script')
